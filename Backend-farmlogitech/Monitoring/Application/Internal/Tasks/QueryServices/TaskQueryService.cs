2d46689f98f39b06d3fad7fadc63230693efeac2
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-07 23:00:04 -0500

    feat(monitoring): query service implemented

---
+++ Backend-farmlogitech/Monitoring/Application/Internal/Tasks/QueryServices/TaskQueryService.cs
@@ -0,0 +1,23 @@
+using Backend_farmlogitech.Monitoring.Domain.Model.Queries.Task;
+using Backend_farmlogitech.Monitoring.Domain.Repositories.Tasks;
+using Backend_farmlogitech.Monitoring.Domain.Services.Tasks;
+using AggregatesTask = Backend_farmlogitech.Monitoring.Domain.Model.Aggregates.Task;
+
+namespace Backend_farmlogitech.Monitoring.Application.Internal.Tasks.QueryServices;
+
+public class TaskQueryService (ITaskRepository taskRepository): ITaskQueryService
+{
+    public async Task<AggregatesTask> Handle(GetTaskByIdQuery query)
+    {
+        return await taskRepository.FindByIdAsync(query.Id);
+    }
+    public async Task<IEnumerable<AggregatesTask>> Handle(GetAllTasksByCollaboratorId query)
+    {
+        return await taskRepository.FindByCollaboratorIdAsync(query.CollaboratorId);
+    }
+
+    public async Task<IEnumerable<AggregatesTask>> Handle(GetAllTasksByCollaboratorIdAndFarmerId query)
+    {
+        return await taskRepository.FindByCollaboratorIdAndFarmerIdAsync(query.CollaboratorId, query.FarmerId);
+    }
+}
