8285d3fd303d09b2d2321591c475910572aa8836
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-08 20:20:26 -0500

    feat(monitoring): command and query services implemented.

---
+++ Backend-farmlogitech/Monitoring/Domain/Services/Messages/IMessageQueryService.cs
@@ -0,0 +1,13 @@
+using Backend_farmlogitech.Monitoring.Domain.Model.Aggregates;
+using Backend_farmlogitech.Monitoring.Domain.Model.Queries.Message;
+
+namespace Backend_farmlogitech.Monitoring.Domain.Services.Messages;
+
+public interface IMessageQueryService
+{
+    Task<Message> Handle(GetMessageByIdQuery query);
+        
+    Task<IEnumerable<Message>> Handle(GetAllMessagesByCollaboratorId query);
+        
+    Task<IEnumerable<Message>> Handle(GetAllMessageQuery query);
+}
