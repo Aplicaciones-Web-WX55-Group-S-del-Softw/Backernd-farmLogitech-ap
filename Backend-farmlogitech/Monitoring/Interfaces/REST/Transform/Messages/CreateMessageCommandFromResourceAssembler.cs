2ff2e0f9614616e14290ee4fd2855933ac579a85
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-08 20:20:54 -0500

    feat(monitoring): transform implemented.

---
+++ Backend-farmlogitech/Monitoring/Interfaces/REST/Transform/Messages/CreateMessageCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+using Backend_farmlogitech.Monitoring.Interfaces.REST.Resources.Messages;
+using Backend_farmlogitech.Monitoring.Domain.Model.Commands.Messages;
+
+namespace Backend_farmlogitech.Monitoring.Interfaces.REST.Transform.Messages
+{
+    public static class CreateMessageCommandFromResourceAssembler
+    {
+        public static CreateMessageCommand ToCommandFromResource(CreateMessageResource resource)
+        {
+            return new CreateMessageCommand(resource.Id, resource.CollaboratorId, resource.Description);
+        }
+    }
+}
