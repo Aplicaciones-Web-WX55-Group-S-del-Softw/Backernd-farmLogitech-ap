5d8d582d3e90c8f5f9889b21e0da93811532edbb
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-08 20:20:13 -0500

    feat(monitoring): imessagerepository implemented.

---
+++ Backend-farmlogitech/Monitoring/Domain/Repositories/Messages/IMessageRepository.cs
@@ -0,0 +1,13 @@
+using backend_famLogitech_aw.Shared.Domain.Repositories;
+using Backend_farmlogitech.Monitoring.Domain.Model.Aggregates;
+
+namespace Backend_farmlogitech.Monitoring.Domain.Repositories;
+
+public interface IMessageRepository: IBaseRepository<Message>
+{
+    Task<IEnumerable<Message>> FindByCollaboratorId(int userId);
+    
+    Task<IEnumerable<Message>> FindByAllMessage();
+    
+    Task<Message> FindByIdx(int id);
+}
