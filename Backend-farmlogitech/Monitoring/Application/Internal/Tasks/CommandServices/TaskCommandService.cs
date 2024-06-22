ff2c50e8093e65080ab42b8ba18c39c0ee0d9e2c
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-22 00:23:36 -0500

    feat(monitoring): task updated.

--- Backend-farmlogitech/Monitoring/Application/Internal/Tasks/CommandServices/TaskCommandService.cs
+++ Backend-farmlogitech/Monitoring/Application/Internal/Tasks/CommandServices/TaskCommandService.cs
@@ -1,29 +1,58 @@
+using System;
+using System.Threading.Tasks;
 using backend_famLogitech_aw.Shared.Domain.Repositories;
+using AggregatesTask = Backend_farmlogitech.Monitoring.Domain.Model.Aggregates.Task;
 using Backend_farmlogitech.Monitoring.Domain.Model.Commands.Tasks;
 using Backend_farmlogitech.Monitoring.Domain.Repositories.Tasks;
 using Backend_farmlogitech.Monitoring.Domain.Services.Tasks;
-using AggregatesTask = Backend_farmlogitech.Monitoring.Domain.Model.Aggregates.Task;
+using Backend_farmlogitech.IAM.Domain.Model.Aggregates;
+using Backend_farmlogitech.IAM.Domain.Model.ValueObjects;
+using Backend_farmlogitech.IAM.Domain.Repositories;
+using static Backend_farmlogitech.IAM.Domain.Model.Aggregates.User;
 
-namespace Backend_farmlogitech.Monitoring.Application.Internal.Tasks.CommandServices;
+namespace Backend_farmlogitech.Monitoring.Application.Internal.CommandServices;
 
-public class TaskCommandService (IUnitOfWork unitOfWork, ITaskRepository taskRepository): ITaskCommandService
+public class TaskCommandService : ITaskCommandService
 {
+    private readonly IUnitOfWork unitOfWork;
+    private readonly ITaskRepository taskRepository;
+    private readonly IUserRepository userRepository;
+
+    public TaskCommandService(IUnitOfWork unitOfWork, ITaskRepository taskRepository, IUserRepository userRepository)
+    {
+        this.unitOfWork = unitOfWork;
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+    }
+
     public async Task<AggregatesTask> Handle(CreateTaskCommand command)
     {
-        var taskNew = await taskRepository.FindByIdx(command.Id);
-        if (taskNew != null)
-            throw new System.Exception("Task with ID already exists");
-        taskNew = new AggregatesTask(command);
-        await taskRepository.AddAsync(taskNew);
+        var userGlobal = User.UserAuthenticate.UserId;
+        var userRole = await userRepository.GetUserRole(userGlobal);
+        if (userRole.Role != Role.FARMER)
+        {
+            throw new Exception("Only users with role FARMER can create a task");
+        }
+
+        // Check if the task already exists
+        var existingTask = await taskRepository.FindByIdx(command.Id);
+        if (existingTask != null)
+        {
+            throw new Exception("Task with this ID already exists");
+        }
+
+        var newTask = new AggregatesTask(command);
+        await taskRepository.AddAsync(newTask);
         await unitOfWork.CompleteAsync();
-        return taskNew;
+        return newTask;
     }
 
     public async Task<AggregatesTask> Handle(UpdateTaskCommand command)
     {
+        var userGlobal = User.UserAuthenticate.UserId; 
         var taskToUpdate = await taskRepository.FindByIdx(command.Id);
         if (taskToUpdate == null)
-            throw new System.Exception("Task with ID does not exist");
+            throw new Exception("Task with ID does not exist");
         taskToUpdate.Update(command);
         await unitOfWork.CompleteAsync();
         return taskToUpdate;
--- Backend-farmlogitech/Monitoring/Domain/Model/Aggregates/Task.cs
+++ Backend-farmlogitech/Monitoring/Domain/Model/Aggregates/Task.cs
@@ -1,34 +1,66 @@
+using System;
 using Backend_farmlogitech.Monitoring.Domain.Model.Commands.Tasks;
 
 namespace Backend_farmlogitech.Monitoring.Domain.Model.Aggregates;
 
 public class Task
 {
-    public int Id { get; private set; }
-    public int CollaboratorId { get; set; }
-    public int FarmerId { get; set; }
-    public string Description { get; set; }
-
-    protected Task()
+    protected Task(int id, int collaboratorId, int farmerId, string description)
     {
-        this.Id = 0;
-        this.CollaboratorId = 0;
-        this.FarmerId = 0;
-        this.Description = string.Empty;
+        this.id = 0;
+        this.collaboratorId = 0;
+        this.farmerId = 0;
+        this.description = string.Empty;
+    }
+    
+    private string _description;
+    private int _collaboratorId;
+
+    public int id { get; private set; }
+    public int farmerId { get; set; }
+
+    public int collaboratorId
+    {
+        get { return _collaboratorId; }
+        set
+        {
+            if (value == 0)
+            {
+                throw new Exception("Collaborator ID cannot be null.");
+            }
+            _collaboratorId = value;
+        }
     }
 
+    public string description
+    {
+        get { return _description; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("Description cannot be null or empty.");
+            }
+            if (value.Length > 30)
+            {
+                throw new Exception("Description is too long. It cannot be more than 30 characters.");
+            }
+            _description = value;
+        }
+    }
+    
     public Task(CreateTaskCommand command)
     {
-        this.Id = command.Id;
-        this.CollaboratorId = command.CollaboratorId;
-        this.FarmerId = command.FarmerId;
-        this.Description = command.Description;
+        this.id = command.Id;
+        this.collaboratorId = command.CollaboratorId;
+        this.farmerId = command.FarmerId;
+        this.description = command.Description;
     }
 
     public void Update(UpdateTaskCommand command)
     {
-        this.CollaboratorId = command.CollaboratorId;
-        this.FarmerId = command.FarmerId;
-        this.Description = command.Description;
+        this.collaboratorId = command.CollaboratorId;
+        this.farmerId = command.FarmerId;
+        this.description = command.Description;
     }
 }
