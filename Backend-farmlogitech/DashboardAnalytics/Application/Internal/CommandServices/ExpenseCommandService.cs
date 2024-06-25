ebc345470cc1f797235b40a68e6da4429b18a029
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-25 03:43:13 -0500

    feat(dashboard-analytics): expenses updated.

--- Backend-farmlogitech/DashboardAnalytics/Application/Internal/CommandServices/ExpenseCommandService.cs
+++ Backend-farmlogitech/DashboardAnalytics/Application/Internal/CommandServices/ExpenseCommandService.cs
@@ -3,6 +3,9 @@ using Backend_farmlogitech.DashboardAnalytics.Domain.Model.Aggregates.Expenses;
 using Backend_farmlogitech.DashboardAnalytics.Domain.Model.Commands.Expenses;
 using Backend_farmlogitech.DashboardAnalytics.Domain.Repositories.Expenses;
 using Backend_farmlogitech.DashboardAnalytics.Domain.Services;
+using Backend_farmlogitech.IAM.Domain.Model.Aggregates;
+using Backend_farmlogitech.IAM.Domain.Model.ValueObjects;
+using Backend_farmlogitech.IAM.Domain.Repositories;
 
 namespace Backend_farmlogitech.DashboardAnalytics.Application.Internal.CommandServices;
 
@@ -10,20 +13,32 @@ public class ExpenseCommandService : IExpenseCommandService
 {
     private readonly IExpenseRepository _expenseRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserRepository userRepository;
 
-    public ExpenseCommandService(IExpenseRepository expenseRepository, IUnitOfWork unitOfWork)
+    public ExpenseCommandService(IExpenseRepository expenseRepository, IUnitOfWork unitOfWork, IUserRepository userRepository)
     {
         _expenseRepository = expenseRepository;
         _unitOfWork = unitOfWork;
+        this.userRepository = userRepository;
     }
 
     public async Task<Expense> Handle(CreateExpenseCommand command)
     {
-        var expense1 = await _expenseRepository.GetByCategoryAndDate(
-            command.Category, command.Date);
-        if(expense1 != null)
+        var userGlobal = User.GlobalVariables.UserId;
+        var userRole = await userRepository.GetUserRole(userGlobal);
+        if (userRole.Role != Role.FARMER)
+        {
+            throw new Exception("Only users with role FARMER can create an expense");
+        }
+
+        // Check if the user has already created an expense
+        var existingExpense = await _expenseRepository.GetByCategoryAndDate(command.Category, command.Date);
+        if(existingExpense != null)
+        {
             throw new Exception("Expense already exists");
-        expense1 = new Expense(command);
+        }
+
+        var expense1 = new Expense(command);
         await _expenseRepository.AddAsync(expense1);
         await _unitOfWork.CompleteAsync();
         return expense1;
--- Backend-farmlogitech/DashboardAnalytics/Application/Internal/QueryServices/ExpenseQueryService.cs
+++ Backend-farmlogitech/DashboardAnalytics/Application/Internal/QueryServices/ExpenseQueryService.cs
@@ -2,30 +2,62 @@
 using Backend_farmlogitech.DashboardAnalytics.Domain.Model.Queries.Expenses;
 using Backend_farmlogitech.DashboardAnalytics.Domain.Repositories.Expenses;
 using Backend_farmlogitech.DashboardAnalytics.Domain.Services;
+using Backend_farmlogitech.IAM.Domain.Model.Aggregates;
+using Backend_farmlogitech.IAM.Domain.Model.ValueObjects;
+using Backend_farmlogitech.IAM.Domain.Repositories;
 
 namespace Backend_farmlogitech.DashboardAnalytics.Application.Internal.QueryServices;
 
 public class ExpenseQueryService : IExpenseQueryService
 {
     private readonly IExpenseRepository _expenseRepository;
+    private readonly IUserRepository userRepository;
 
-    public ExpenseQueryService(IExpenseRepository expenseRepository)
+    public ExpenseQueryService(IExpenseRepository expenseRepository, IUserRepository userRepository)
     {
         _expenseRepository = expenseRepository;
+        this.userRepository = userRepository;
     }
 
     public async Task<Expense> Handle(GetExpenseByIdQuery query)
     {
-        return await _expenseRepository.GetExpenseById(query.Id);
+        var expense = await _expenseRepository.GetExpenseById(query.Id);
+
+        var userGlobal = User.GlobalVariables.UserId;
+        var userRole = await userRepository.GetUserRole(userGlobal);
+        if (userRole.Role != Role.FARMER || expense.FarmId != userGlobal)
+        {
+            throw new Exception("You can only view expenses that you have created");
+        }
+
+        return expense;
     }
 
     public async Task<Expense?> Handle(GetExpenseByCategoryAndDateQuery query)
     {
-        return await _expenseRepository.GetByCategoryAndDate(query.Category, query.Date);
+        var expense = await _expenseRepository.GetByCategoryAndDate(query.Category, query.Date);
+
+        var userGlobal = User.GlobalVariables.UserId;
+        var userRole = await userRepository.GetUserRole(userGlobal);
+        if (userRole.Role != Role.FARMER || expense.FarmId != userGlobal)
+        {
+            throw new Exception("You can only view expenses that you have created");
+        }
+
+        return expense;
     }
 
     public async Task<IEnumerable<Expense>> Handle(GetAllByFarmIdQuery query)
     {
-        return await _expenseRepository.GetAllByFarmId(query.Farmid);
+        var expenses = await _expenseRepository.GetAllByFarmId(query.Farmid);
+
+        var userGlobal = User.GlobalVariables.UserId;
+        var userRole = await userRepository.GetUserRole(userGlobal);
+        if (userRole.Role != Role.FARMER || expenses.Any(expense => expense.FarmId != userGlobal))
+        {
+            throw new Exception("You can only view expenses that you have created");
+        }
+
+        return expenses;
     }
 }
